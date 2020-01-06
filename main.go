0dbcb38a84b88a4503b05371e50823264d712f98
Neema Sadry <snake117@gmail.com>
2020-01-05 21:23:32 -0500

    Changed main.go and configured runner.conf file.

--- main.go
+++ main.go
@@ -1,24 +1,29 @@
-package main
+package main // First thing that starts running your program
 
 import (
-	"fmt"
-	"net/http"
+	"fmt" // used for printing out data to different writers or standard-out
+	"net/http" // for handling/creating web requests and running server
 )
 
 
 // This function executes anytime someone visits our web app
 // ResponseWriter is part of http package and an interface we take in
 // *http.Request is a pointer
-// handlerFunc can be named anything actually
+// DOES NOT NEED TO BE CALLED handlerFunc, can be named anything actually
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Someone visited our page") // prints out in the logs when fresh is running (ie does NOT print in webpage)
 	// first argument is where you want it to print to
 	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc) 
+	http.HandleFunc("/", handlerFunc) // we're NOT calling the function, just telling it to use this function
+	// as long as URL path matches the PREFIX it will use the handlerFunc
+
+
 	http.ListenAndServe(":3000", nil) // localhost is implied; nil means use the built-in serve-mux that comes with the built-in http package
-	// ListenAndServe starts up the web server
+	// ListenAndServe starts up the web server and to listen on port 3000
+	// If you put localhost:3000, then the request is restricted to only localhost, whereas this way can be used with anything as long it goes through port :3000
 	// unencrypted web traffic listens on Port 80 by default
 	
 }
