2a43edee1a3f84e7897087f749fe168cd7965586
Neema Sadry <snake117@gmail.com>
2020-01-05 21:52:05 -0500

    Added a contact page.

--- main.go
+++ main.go
@@ -14,7 +14,11 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	// fmt.Println("Someone visited our page") prints out in the logs when fresh is running (ie does NOT print in webpage)
 	// first argument is where you want it to print to
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:support@lenslocked.com\">support@lenslocked.com</a>")
+	}
 }
 
 func main() {
