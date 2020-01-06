d8ec5923e7d954ee06f37645c26671902ecec89c
Neema Sadry <snake117@gmail.com>
2020-01-05 19:42:04 -0500

    Created our first web server

--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+
+// This function executes anytime someone visits our web app
+// ResponseWriter is part of http package and an interface we take in
+// *http.Request is a pointer
+// handlerFunc can be named anything actually
+func handlerFunc(w http.ResponseWriter, r *http.Request) {
+	// first argument is where you want it to print to
+	fmt.Fprint(w, "<h1>Welcome to my awesome site!</h1>")
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	http.HandleFunc("/", handlerFunc) 
+	http.ListenAndServe(":3000", nil) // localhost is implied; nil means use the built-in serve-mux that comes with the built-in http package
+	// ListenAndServe starts up the web server
+	// unencrypted web traffic listens on Port 80 by default
+	
 }
